0849c186fa36a55757dbcc9d6aa6e27f5d0f5a7b
Himzzzz :) <101552401+HeyHimzz@users.noreply.github.com>
2022-03-14 15:36:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, User, Message
 
 Bot = Client(
-     session_name="Profile-Bot",
+     session_name="ProfileBot",
      api_id=API_ID,
      api_hash=API_HASH,
      bot_token=BOT_TOKEN
